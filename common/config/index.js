a4775c4cf0abd835a7de626056285beec8531971
WangXiaoXi <995107408@qq.com>
2019-07-15 06:35:17 +0800

    add

--- common/config/index.js
+++ common/config/index.js
@@ -3,6 +3,7 @@
  */
 let url_config = ''
 
+// #ifndef H5
 if(process.env.NODE_ENV === 'development') {
     // 开发环境
     url_config = 'http://hzyh.ehanone.com'
@@ -10,5 +11,6 @@ if(process.env.NODE_ENV === 'development') {
     // 生产环境
     url_config = 'http://hzyh.ehanone.com'
 }
+// #endif
 
 export const url_base = url_config
