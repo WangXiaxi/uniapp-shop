dc8d40c9bc44e49965ac7f510a7eac52a2bcde3f
wangxiaoxing <995107408@qq.com>
2019-08-27 13:21:03 +0800

    add

--- pages/phoneView/phoneView.vue
+++ pages/phoneView/phoneView.vue
@@ -14,28 +14,36 @@
 		},
 		methods: {
 			loadData() {
-				 mineModel.getPhoneWhitelist().then(res => {
-					 const phone = res.data.phone.map(c => {
-						c.value = c.phone
-						delete c.phone
-						return c
-					})
-					 let page = Math.ceil(phone.length / 400) // 计算页码
-					 plus.contacts.getAddressBook(plus.contacts.ADDRESSBOOK_PHONE, (addressbook) => {
-					 	addressbook.find(['displayName', 'phoneNumbers'], (contacts) => {
+				plus.contacts.getAddressBook(plus.contacts.ADDRESSBOOK_PHONE, (addressbook) => {
+					mineModel.getPhoneWhitelist().then(res => {
+						const phone = res.data.phone.map(c => {
+							c.value = c.phone
+							delete c.phone
+							return c
+						})
+						let page = Math.ceil(phone.length / 400) // 计算页码
+						addressbook.find(['displayName', 'phoneNumbers'], (contacts) => {
 							for (let i = 0; i < page; i++) {
-								console.log(i)
 								const contact = addressbook.create()
-								contact.name = { givenName : (i === 1 ? '接听忆杭网的来电' : '接听忆杭网来电') }
+								contact.name = {
+									givenName: (i === 1 ? '接听忆杭网的来电' : '接听忆杭网来电')
+								}
 								contact.phoneNumbers = phone.slice(i * 400, (i + 1) * 400)
 								contact.save()
 							}
-					 	}, function() {
-					 	}, {
-					 		multiple: true
-					 	})
-					 })
-				 })
+						}, function() {}, {
+							multiple: true
+						})
+					})
+				}, () => {
+					uni.showModal({
+					    title: '提示',
+					    content: '获取通讯录权限失败，请前往设置允许访问通讯录权限！',
+					    complete: function (res) {
+					       uni.navigateBack()
+					    }
+					});
+				})
 			}
 		}
 	}
